944be556f13ddc50251e29513715727813c827e8
NivedhaVijayakumar <84761947+NivedhaVijayakumar@users.noreply.github.com>
2021-05-30 19:56:05 +0530

    Add files via upload

---
+++ Nivedha/Rectangle.java
@@ -0,0 +1,23 @@
+class Nivedha_Rectangle{
+	public static void main(String args[]){
+	 //area
+	 
+		    int length = 8;
+			double breadth = 3.06;
+			double area = length*breadth;
+			System.out.println("Area of Rectangle:"+area);
+	}
+	  //circumference
+	  {int length = 5;
+			int breadth = 3;
+			int circumference = 2*(length*breadth);
+			System.out.println("circumference of Rectangle:"+circumference);
+	  }
+	  
+}
+	  
+	  
+	
+
+	
+	
