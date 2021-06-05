89950763802916d5107b044e059c7ec76c202f8d
NivedhaVijayakumar <Nivedha_Chinni.Vijayakumar_Balakrishnan@schwalltech.in>
2021-06-05 19:13:54 +0530

    Update

--- Nivedha/1-June-2021/Stringnew.java
+++ Nivedha/1-June-2021/Stringnew.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class Stringnew{
 	public static void main(String[] args){
 	Scanner sc = new Scanner(System.in);
-	String random=sc.next()
+	String random=sc.next();
 	System.out.println(random.replace("the","The"));
 	System.out.println(random.split("."));}
 }
