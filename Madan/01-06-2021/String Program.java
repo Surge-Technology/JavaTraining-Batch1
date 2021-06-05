c1f441eb674ae292261221a6cd75f7a09be7ef18
Madankumar R <Madankumar_Rajasekar@schwalltech.in>
2021-06-05 12:18:47 +0530

    Programming task
    programs for
    Array,
    pattern,
    string function,
    prime number,
    matrix multiplication,
    polymorphism and inheritance program.

---
+++ Madan/01-06-2021/String Program.java
@@ -0,0 +1,16 @@
+
+public class Madan {
+
+
+public static void main(String[] args) {
+char DoubleQuotes ='"';
+String value= "Earth is the third planet from the Sun and the only astronomical object known to harbor and support life. About 29.2% of Earth's surface is land consisting of continents and islands. The remaining 70.8% is covered with water, mostly by oceans, seas, gulfs, and other salt-water bodies, but also by lakes, rivers, and other freshwater, which together constitute the hydrosphere. Much of Earth's polar regions are covered in ice. Earth's outer layer is divided into several rigid tectonic plates that migrate across the surface over many millions of years, while its interior remains active with a solid iron inner core, a liquid outer core that generates Earth's magnetic field, and a convective mantle that drives plate tectonics.";
+
+value= value.replace("29.2", DoubleQuotes+"29.2"+DoubleQuotes);
+value= value.replace("70.8", DoubleQuotes+"70.8"+DoubleQuotes);
+System.out.println(value.replaceAll("the","The"));
+
+}
+
+}
+
---
+++ Madan/31-05-2021/pattern3.java
@@ -0,0 +1,17 @@
+
+public class Pattern3 {
+
+		public static void main(String []arg) {
+		for(int i=1;i<=3;i++) {
+		for(int k=3-1;k>=i;k--) {
+		System.out.print(" ");
+		}
+		for(int j=1;j<=i;j++) {
+		System.out.print("* ");
+
+		}
+		System.out.print("\n");
+		}
+		}
+		}
+
