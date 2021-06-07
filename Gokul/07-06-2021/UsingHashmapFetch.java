324bdb296a58c91cb0ab1c5db7ae7b56f8f7e842
GokulBalajiM P <gokul_balaji.muruganandam@schwalltech.in>
2021-06-07 20:00:34 +0530

    7-6-2021

---
+++ Gokul/07-06-2021/UsingHashmapFetch.java
@@ -0,0 +1,12 @@
+package hashmap;
+
+
+
+public class UsingHashmapFetch {
+
+public static void main(String[] args) {
+		
+		Studentdetails studentdetails = new Studentdetails();
+		studentdetails.add();
+	}
+}
