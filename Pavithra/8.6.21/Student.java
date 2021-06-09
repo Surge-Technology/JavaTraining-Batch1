8333804d556cfbde89cc5cd9fdb4e747ee6b13fd
pavithra-776 <Pavithra.Chandrasekaran@schwalltech.in>
2021-06-10 01:28:46 +0530

    Hashtable

---
+++ Pavithra/8.6.21/Student.java
@@ -0,0 +1,25 @@
+package Hashtable;
+
+public class Student {
+
+	int id;
+	String name;
+	String address;
+	
+	
+ Student()
+	{
+this.id=id;
+this.name=name;
+this.address=address;
+}
+ @Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", address=" + address + "]";
+
+}
+
+
+	}
+
+
