094d14148a6cf80b019a3f3a15d8ca20cfbecb97
GokulBalajiM P <gokul_balaji.muruganandam@schwalltech.in>
2021-06-09 09:41:07 +0530

    8-6-2021

---
+++ Gokul/08-06-2021/Student.java
@@ -0,0 +1,56 @@
+package hashtable;
+
+public class Student {
+	 int id;
+	 String name;
+	 String dob;
+	
+	
+	
+	
+	Student()
+	{
+		
+	}
+	
+	Student(int id,String name, String dob) {
+		this.id = id;
+		this.name = name;
+		this.dob = dob;
+		
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	
+	
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", dob=" + dob + "]";
+	}
+
+}
+
---
+++ Gokul/08-06-2021/UsingHashtableFetch.java
@@ -0,0 +1,11 @@
+package hashtable;
+
+import hashmap.Studentdetails;
+
+public class UsingHashtableFetch {
+public static void main(String[] args) {
+		
+	Studentdetail st = new Studentdetail();
+	st.add1();
+	}
+}
