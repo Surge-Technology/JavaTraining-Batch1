0280346dd8a282e245075645cfbb3f2620f5a268
Yuvarajan-M <Yuvarajan.Murugan@schwalltech.in>
2021-09-05 22:18:34 +0530

    curd.java

---
+++ Yuvaraj/practice-2/src/main/java/com/example/practice/entities/Employee.java
@@ -0,0 +1,14 @@
+package com.example.practice.entities;
+
+import java.util.ArrayList;
+
+public class Employee {
+	int id;
+	String name;
+	String mobNo;
+	
+
+}
+class Bd{
+	ArrayList <Employee> list=new ArrayList<>();
+}
