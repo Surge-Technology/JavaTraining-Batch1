62e2a49b9538726f7f1f734872b3f3ef64c2c6b0
MNaveenraj <89304735+MNaveenraj@users.noreply.github.com>
2021-12-10 13:14:52 +0530

    camunda

--- Naveenraj/camunda db flow/src/main/java/com/example/workflow/entity/EmpTable.java
+++ Naveenraj/camunda db flow/src/main/java/com/example/workflow/entity/EmpTable.java
@@ -1,52 +1,39 @@
-package com.example.workflow.entity;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-
-@Entity
-@Table(name="emp_details")
-public class EmpTable {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
-	private Long id;
-	
-	
-	@Column
-	private String name;
-	
-	@Column
-	private String mail;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getMail() {
-		return mail;
-	}
-
-	public void setMail(String mail) {
-		this.mail = mail;
-	}
-
-	
-	
-}
+/*
+ * package com.example.workflow.entity;
+ * 
+ * import javax.persistence.Column; import javax.persistence.Entity; import
+ * javax.persistence.GeneratedValue; import javax.persistence.GenerationType;
+ * import javax.persistence.Id; import javax.persistence.Table;
+ * 
+ * 
+ * @Entity
+ * 
+ * @Table(name="emp_details") public class EmpTable {
+ * 
+ * @Id
+ * 
+ * @GeneratedValue(strategy = GenerationType.IDENTITY)
+ * 
+ * @Column(name = "id") private Long id;
+ * 
+ * 
+ * @Column private String name;
+ * 
+ * @Column private String mail;
+ * 
+ * public Long getId() { return id; }
+ * 
+ * public void setId(Long id) { this.id = id; }
+ * 
+ * public String getName() { return name; }
+ * 
+ * public void setName(String name) { this.name = name; }
+ * 
+ * public String getMail() { return mail; }
+ * 
+ * public void setMail(String mail) { this.mail = mail; }
+ * 
+ * 
+ * 
+ * }
+ */
